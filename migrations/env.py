d04d44c883a44046e278d9abe14107024363861f
Anton Karol <work.antonkarol@gmail.com>
2024-12-02 23:59:15 +0300

    edited migrations env file

--- migrations/env.py
+++ migrations/env.py
@@ -5,18 +5,18 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.config import DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME
+from src.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_PORT", DB_PORT)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_PASS", DB_PASS)
-config.set_section_option(section, "DB_NAME", DB_NAME)
+config.set_section_option(section, "DB_HOST", settings.DB_HOST)
+config.set_section_option(section, "DB_PORT", settings.DB_PORT)
+config.set_section_option(section, "DB_USER", settings.DB_USER)
+config.set_section_option(section, "DB_PASS", settings.DB_PASS)
+config.set_section_option(section, "DB_NAME", settings.DB_NAME)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
